5c3962c268f2c4d4e0a25ae5164fe70e73ebc360
Benjamin Lau <capeguy@capeguy.net>
2023-05-08 19:06:14 +0000

    Updates

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -61,16 +61,19 @@ const Hero = () => {
 
   const one = <h1>Hi, my name is</h1>;
   const two = <h2 className="big-heading">Benjamin Lau.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const three = <h3 className="big-heading">I build Systems & Teams.</h3>;
   const four = (
     <>
+      <p>(WORK-IN-PROGRESS)</p>
       <p>
-        (WORK-IN-PROGRESS)
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
+        I’m a software engineer at heart, specializing in building and designing scalable systems
+        and teams for enterprises.
+      </p>
+      <p>
+        Currently, I’m focused on helping clients achieve digital transformation through the
+        delivery of top-quality client-centric solutions to high-profile organizations at{' '}
+        <a href="https://emq2.io/" target="_blank" rel="noreferrer">
+          EMQ2
         </a>
         .
       </p>
@@ -79,10 +82,11 @@ const Hero = () => {
   const five = (
     <a
       className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
+      href="https://www.linkedin.com/in/benjamin-ben-lau/"
       target="_blank"
-      rel="noreferrer">
-      Check out my course!
+      rel="noreferrer"
+    >
+      Connect with me on LinkedIn!
     </a>
   );
 
