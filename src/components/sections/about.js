d317113c042cfaa8d7affae1f3f3091a890ae24c
Benjamin Lau <capeguy@capeguy.net>
2025-10-18 16:00:24 +0800

    Updated image

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -196,7 +196,7 @@ const About = () => {
           <div className="wrapper">
             <StaticImage
               className="img"
-              src="../../images/me.png"
+              src="../../images/me.jpg"
               width={500}
               quality={95}
               formats={['AUTO', 'WEBP', 'AVIF']}
