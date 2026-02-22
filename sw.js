2241a9bc38cc8ed915e80e4f1db6d4c39988e688
Benjamin Lau <capeguy@users.noreply.github.com>
2026-02-22 13:39:28 +0000

    GitHub Pages Auto Deploy

--- sw.js
+++ sw.js
@@ -27,23 +27,23 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-2a1145c26e9f17cb0c16.js"
+    "url": "webpack-runtime-dde6013a614604b2ceb1.js"
   },
   {
     "url": "framework-fbf2011d6c2b83ba228a.js"
   },
   {
-    "url": "app-8b3a7a2a5724200fb0f5.js"
+    "url": "app-886633a5df5f82938eda.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "e440f3a7669fc9a30375e022d6da26d1"
+    "revision": "7e64aa259d81d385fb76ee2d50869b0f"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-e84596c37790733d100e.js"
   },
   {
-    "url": "polyfill-150da4ea4fb9cd5b834e.js"
+    "url": "polyfill-677433b5b030576335de.js"
   },
   {
     "url": "manifest.webmanifest",
@@ -139,7 +139,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-8b3a7a2a5724200fb0f5.js`))) {
+  if (!resources || !(await caches.match(`/app-886633a5df5f82938eda.js`))) {
     return await fetch(event.request)
   }
 
